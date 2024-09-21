bb3d9c1c5fbe2ed09f1a1430c07ce0828a9fbf44
Maria Estrella <98559654+mlestrella843@users.noreply.github.com>
2024-09-21 08:01:32 -0600

    destructuring Arrays

---
+++ destruct_arrays.js
@@ -0,0 +1,11 @@
+let names = ['Dylan', 'coding good', 'Israel'];
+
+// console.log(names);
+
+let [ firstName, middleName, lastName ] = ['Dylan', 'coding good', 'Israel'];
+console.log(firstName);
+
+// you can overwrite the value like this.
+
+firstName = "Maria"
+console.log( firstName + " " + lastName);
